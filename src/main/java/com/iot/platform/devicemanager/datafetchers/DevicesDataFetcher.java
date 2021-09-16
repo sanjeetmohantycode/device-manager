2e1abdd242b558ef3c467015db74469cfc9f8c0c
Sanjeet Mohanty <sanjeet215@gmail.com>
2021-09-16 09:50:46 +0530

    [sanjeet] query with parameter fixed.

--- src/main/java/com/iot/platform/devicemanager/datafetchers/DevicesDataFetcher.java
+++ src/main/java/com/iot/platform/devicemanager/datafetchers/DevicesDataFetcher.java
@@ -7,12 +7,15 @@ import com.iot.platform.devicemanager.service.DeviceService;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import com.netflix.graphql.dgs.DgsMutation;
+import com.netflix.graphql.dgs.DgsQuery;
 import com.netflix.graphql.dgs.InputArgument;
 import graphql.schema.DataFetchingEnvironment;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @DgsComponent
+@Slf4j
 public class DevicesDataFetcher {
 
     private final DeviceService deviceService;
@@ -23,7 +26,14 @@ public class DevicesDataFetcher {
 
     @DgsData(parentType = "Query", field = "devices")
     public List<Device> getDevices(DataFetchingEnvironment dataFetchingEnvironment) {
+        log.info("Fetching all devices");
         return new ArrayList<>(deviceService.getAllDevicesPresentInSystem());
+    }
+
+    @DgsQuery(field = "deviceList")
+    public List<Device> getDevicesByOrganization(@InputArgument String orgId) {
+        log.info("Fetching data from devices by organization with orgId: {}", orgId);
+        return new ArrayList<>(deviceService.getAllDevicesByOrganization(orgId));
     }
 
     @DgsMutation
